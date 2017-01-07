b569bdb5334ae2bd5b96655202154b794dc8cb7b
Aliaksandr Salauyou <asolovyev@iitdgroup.ru>
2017-01-07 17:21:33 +0300

    fixed formatting

--- src/main/java/ru/salauyou/builder/api/Builder.java
+++ src/main/java/ru/salauyou/builder/api/Builder.java
@@ -5,14 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Builder {
 
-	Class<?>[] appliedTo() default {};
-	
-	Class<?>[] dependsOn() default {};
-	
-	boolean stopOnException() default false;
-	
+  Class<?>[] appliedTo() default {};
+
+  Class<?>[] dependsOn() default {};
+
+  boolean stopOnException() default false;
+
 }
--- src/main/java/ru/salauyou/builder/util/TreeHelper.java
+++ src/main/java/ru/salauyou/builder/util/TreeHelper.java
@@ -11,70 +11,71 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 
+
 public final class TreeHelper {
 
-	
-	/**
-	 * Returns a list of topologycally sorted vertices
-	 * of a given graph. Graph is presented as multimap
-	 * where each entry represents an edge directed
-	 * from key to value
-	 * 
-	 * @throws IllegalArgumentException if graph 
-	 * 		is not a DAG
-	 */
-	public static <E> List<E> topologicalSort(
-			Multimap<E, E> graph) {
-		
-		// initialize
-	  // reversed edges
-		Multimap<E, E> m = HashMultimap.create();
-		// 'current' degree
-		Map<E, Integer> d = Maps.newHashMap();  
-		
-		for (E e : graph.keySet()) {
-			Collection<? extends E> vs = graph.get(e);
-			d.put(e, vs.size());
-			for (E v : vs) {
-				if (!d.containsKey(v)) {
-					d.put(v, 0);
-				}
-				m.put(v, e);
-			}
-		}
-		// vertices with degree 0
-		Queue<E> c = Lists.newLinkedList();
-		for (Map.Entry<E, Integer> e : d.entrySet()) {
-			if (e.getValue() == 0) {
-				c.add(e.getKey());
-			}
-		}
-		
-		// process
-		List<E> r = Lists.newArrayList();
-		E e;
-		while ((e = c.poll()) != null) {
-			d.remove(e);
-			r.add(e);
-			for (E v : m.get(e)) {
-				int deg = d.get(v);
-				if (deg == 1) {
-					c.offer(v);
-					d.remove(v);
-				} else {
-					d.put(v, deg - 1);
-				}
-			}
-		}
-		if (!d.isEmpty()) {
-			throw new IllegalArgumentException(
-				"Given graph is not a DAG");
-		} else {
-			Collections.reverse(r);
-			return r;
-		}
-	}
+  /**
+   * Returns a list of topologycally sorted vertices 
+   * of a given graph. Graph is presented as multimap 
+   * where each entry represents an edge directed 
+   * from key to value
+   * 
+   * @throws IllegalArgumentException
+   *    if graph is not a DAG
+   */
+  public static <E> List<E> topologicalSort(
+      Multimap<E, E> graph) {
 
-	
-	private TreeHelper() {}
+    // initialize
+    // reversed edges
+    Multimap<E, E> m = HashMultimap.create();
+    // 'current' degree
+    Map<E, Integer> d = Maps.newHashMap();
+
+    for (E e : graph.keySet()) {
+      Collection<? extends E> vs = graph.get(e);
+      d.put(e, vs.size());
+      for (E v : vs) {
+        if (!d.containsKey(v)) {
+          d.put(v, 0);
+        }
+        m.put(v, e);
+      }
+    }
+    // vertices with degree 0
+    Queue<E> c = Lists.newLinkedList();
+    for (Map.Entry<E, Integer> e : d.entrySet()) {
+      if (e.getValue() == 0) {
+        c.add(e.getKey());
+      }
+    }
+
+    // process
+    List<E> r = Lists.newArrayList();
+    E e;
+    while ((e = c.poll()) != null) {
+      d.remove(e);
+      r.add(e);
+      for (E v : m.get(e)) {
+        int deg = d.get(v);
+        if (deg == 1) {
+          c.offer(v);
+          d.remove(v);
+        } else {
+          d.put(v, deg - 1);
+        }
+      }
+    }
+    if (!d.isEmpty()) {
+      throw new IllegalArgumentException(
+          "Given graph is not a DAG");
+    } else {
+      Collections.reverse(r);
+      return r;
+    }
+  }
+
+
+  private TreeHelper() {}
+  
 }
